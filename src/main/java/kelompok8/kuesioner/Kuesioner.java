f4ec86e602661d9ffa1e14043976674fd88e799c
codejiva <rambatilumbungpardja@gmail.com>
2024-05-26 10:49:54 +0700

    26052024_codejiva_first commit

---
+++ src/main/java/kelompok8/kuesioner/Kuesioner.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kelompok8.kuesioner;
+
+/**
+ *
+ * @author codejiva
+ */
+public class Kuesioner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
